5809800816aa73580bad70848243712b3bfb9e51
ilyas114rise <45353711+ilyas114rise@users.noreply.github.com>
2019-04-28 21:53:13 +0700

    Add files via upload

---
+++ Stream.js
@@ -0,0 +1,65 @@
+function xac(xacName,elmnt,color) {
+  var i, auc, zacs;
+  auc = document.getElementsByClassName("auc");
+  for (i = 0; i < auc.length; i++) {
+    auc[i].style.display = "none";
+  }
+  zacs = document.getElementsByClassName("zac");
+  for (i = 0; i < zacs.length; i++) {
+    zacs[i].style.backgroundColor = "";
+  }
+  document.getElementById(xacName).style.display = "block";
+  elmnt.style.backgroundColor = color;
+
+}
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+</script>
+<script language="JavaScript">
+//Disable right click script 
+function clickIE() {if (document.all) {(message);return false;}} 
+function clickNS(e) {if 
+(document.layers||(document.getElementById&&!document.all)) { 
+if (e.which==2||e.which==3) {(message);return false;}}} 
+if (document.layers) 
+{document.captureEvents(Event.MOUSEDOWN);document.onmousedown=clickNS;} 
+else{document.onmouseup=clickNS;document.oncontextmenu=clickIE;} 
+document.oncontextmenu=new Function("return false") 
+// 
+function disableCtrlKeyCombination(e)
+{
+//list all CTRL + key combinations you want to disable
+var forbiddenKeys = new Array('a','n','c','x', 's', 'j','u');
+var key;
+var isCtrl;
+if(window.event)
+{
+key = window.event.keyCode;     //IE
+if(window.event.ctrlKey)
+isCtrl = true;
+else
+isCtrl = false;
+}
+else
+{
+key = e.which;     //firefox
+if(e.ctrlKey)
+isCtrl = true;
+else
+isCtrl = false;
+}
+//if ctrl is pressed check if other key is in forbidenKeys array
+if(isCtrl)
+{
+for(i=0; i<forbiddenKeys.length; i++)
+{
+//case-insensitive comparation
+if(forbiddenKeys[i].toLowerCase() == String.fromCharCode(key).toLowerCase())
+{
+alert('Key combination CTRL + '+String.fromCharCode(key) +' has been disabled.');
+return false;
+}
+}
+}
+return true;
+}
